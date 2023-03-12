2cc2a8caf0f95deb646c4d7180d70f2749520720
Sławomir Albiński <slawomiralbinski@gmail.com>
2023-03-12 19:47:46 +0100

    full

--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Profile from 'components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
+import Transactions from './components/Transactions/Transactions.jsx';
+
+import user from './components/Profile/user.json';
+import data from './components/Statistics/data.json';
+import friends from './components/FriendList/friends.json';
+import history from './components/Transactions/transactions.json';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <Transactions items={history} />
+    </App>
   </React.StrictMode>
 );
